6cd2dd43461178eab709434be0e4027f1ec24a22
CalebMartim <calebmartim2113@gmail.com>
2022-08-31 10:21:28 -0300

    First commit
    First commit adding the first idea for the game song and the death screen

---
+++ derrota.asm
@@ -0,0 +1,30 @@
+li a7, 31
+li a0, 39
+li a1, 850
+li a2, 0
+li a3, 100
+ecall
+
+#Pause
+li a0, 850 
+li a7, 32
+ecall
+
+li a7, 31
+li a0, 36
+ecall
+
+## Pause
+li a0, 850 
+li a7, 32
+ecall
+
+li a7, 31
+li a0, 30
+li a1, 2000
+ecall
+
+## Pause
+li a0, 850 
+li a7, 32
+ecall
