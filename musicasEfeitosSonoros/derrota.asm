c100e17285bcdbd83082742c913ad5cf761077e7
Caleb <67715898+CalebMartim@users.noreply.github.com>
2022-09-30 18:19:03 -0300

    Fixed formatting

--- musicasEfeitosSonoros/derrota.asm
+++ musicasEfeitosSonoros/derrota.asm
@@ -1,33 +1,31 @@
 .macro PAUSE
-		li a0, 850 
-		li a7, 32
-		ecall
+li a0, 850 
+li a7, 32
+ecall
 .end_macro
 	
 .text
-MAIN:
+MAIN:	li a7, 31
+	li a0, 39
+	li a1, 850
+	li a2, 0
+	li a3, 100
+	ecall
 
-li a7, 31
-li a0, 39
-li a1, 850
-li a2, 0
-li a3, 100
-ecall
+	#Pause
+	PAUSE
 
-#Pause
-PAUSE
+	li a7, 31
+	li a0, 36
+	ecall
 
-li a7, 31
-li a0, 36
-ecall
+	## Pause
+	PAUSE
 
-## Pause
-PAUSE
+	li a7, 31
+	li a0, 30
+	li a1, 2000
+	ecall
 
-li a7, 31
-li a0, 30
-li a1, 2000
-ecall
-
-## Pause
-PAUSE
+	## Pause
+	PAUSE
