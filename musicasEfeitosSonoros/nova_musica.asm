7eb56af3423c52afa2475182aac7f75aa9ea3f68
Caleb <67715898+CalebMartim@users.noreply.github.com>
2022-10-04 18:58:37 -0300

    Create nova_musica.asm

---
+++ musicasEfeitosSonoros/nova_musica.asm
@@ -0,0 +1,50 @@
+##########################
+# a0: nota               #
+# a1: duração            #
+# a2: instrumento        #
+# a3: volume             # 
+##########################
+
+.macro pause
+addi a7, zero, 32  
+addi a0, x0, 500 ## Esperar 1 segundo antes da próxima nota
+ecall
+.end_macro
+
+.text
+MAIN:	li a7, 31
+	li a1, 1000
+	li a2, 0
+	li a0, 40
+	li a3, 127
+	ecall
+	li a0, 59
+	ecall
+	li a0, 64
+	ecall
+	
+	pause
+	
+	li a7, 31
+	li a0, 59
+	ecall
+	li a0, 64
+	ecall
+	
+	pause
+	
+	li a7, 31
+	li a0, 39
+	ecall
+	li a0, 58
+	ecall
+	li a0, 63
+	ecall
+	
+	pause
+	
+	li a7, 31
+	li a0, 58
+	ecall
+	li a0, 63
+	ecall
